fb79a021d9b14aef7a9d794bac625f9a69b1ef64
Andrew Walker <awalker@ixsystems.com>
2023-05-24 14:22:39 -0400

    Fix LD_LIBRARY_PATH

--- truenas_install/__main__.py
+++ truenas_install/__main__.py
@@ -426,7 +426,9 @@ def main():
                             if old_bootfs_prop != "-":
                                 run_command(["zfs", "set", "truenas:12=1", old_bootfs_prop])
 
-                        cp = run_command([f"LD_LIBRARY_PATH={root}", f"{root}/usr/local/bin/truenas-initrd.py", root], check=False)
+                        os.environ['LD_LIBRARY_PATH'] = f'{root}/usr/lib'
+                        cp = run_command([f"{root}/usr/local/bin/truenas-initrd.py", root], check=False)
+                        os.environ['LD_LIBRARY_PATH'] = ''
                         if cp.returncode > 1:
                             raise subprocess.CalledProcessError(
                                 cp.returncode, f'Failed to execute truenas-initrd: {cp.stderr}'
